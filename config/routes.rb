8687daffa7bff03cac7018176b0e521e42e77225
over-geek <kenagy100@gmail.com>
2024-01-30 15:33:35 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Defines the root path route ('/')
+  # root 'posts#index'
 
   resources :recipes, except: [:update]
 end
