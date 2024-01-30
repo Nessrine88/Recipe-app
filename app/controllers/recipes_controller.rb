330f133b27bce8eda1dc7bae34afbffa82a3d493
over-geek <kenagy100@gmail.com>
2024-01-30 14:25:46 +0000

    Add recipe actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,36 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @recipes = current_user.recipes
   end
 
   def show
+    # @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: 'Recipe was successfully created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def set_recipe
+    @recipe = current_user.recipes.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
   end
 end
