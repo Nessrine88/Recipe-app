c9ede831911f83a7ea1ca77b38a52a84715f7875
Franck sefu <francksefu1998@gmail.com>
2024-01-30 18:45:53 +0200

    Update application_controller.rb
    Add name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  class ApplicationController < ActionController::Base
+
     protect_from_forgery with: :exception
 
     before_action :update_allowed_parameters, if: :devise_controller?
@@ -12,5 +12,5 @@ class ApplicationController < ActionController::Base
         u.permit(:name, :email, :password, :current_password)
       end
     end
-  end
+
 end
