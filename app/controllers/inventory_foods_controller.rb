d4575a66f18a5c90708be93031053b908dea3258
over-geek <kenagy100@gmail.com>
2024-02-01 13:46:34 +0000

    Add controller actions

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -1,9 +1,13 @@
 class InventoryFoodsController < ApplicationController
-	before_action :set_inventory
+	before_action :set_inventory, only: [:destroy]
 
 	def index
-    @inventory_foods = @inventory.inventory_foods
-		@foods = @inventory.foods
+    @inventory_foods = InventoryFood.where(inventory_id: @inventory.id)
+  end
+
+  def destroy
+    @inventory_food.destroy
+    redirect_to inventories_path, notice: 'Inventory food was successfully deleted.'
   end
 
 	private
