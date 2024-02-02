3acc13959d3524e093655ebe05a103b8a0ea5087
unknown <macherki.nessrine@yahoo.com>
2024-02-02 08:53:04 +0100

    add general inventory shopping lists

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,56 @@
+class ShoppingListsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @inventory = Inventory.find_by(id: params[:inventory_id])
+    if @recipe && @inventory
+      recipe_food_items = @recipe.recipe_foods
+      inventory_food_items = @inventory.inventory_foods
+
+      @missing_food_items = []
+
+      recipe_food_items.each do |item|
+        inventory_item = inventory_food_items.find { |inventory_item| inventory_item.food == item.food }
+
+        if inventory_item
+          if inventory_item.quantity < item.quantity
+            missing_quantity = item.quantity - inventory_item.quantity
+            missing_item = item.dup
+            missing_item.quantity = missing_quantity
+            @missing_food_items << missing_item
+          end
+        else
+          @missing_food_items << item.dup
+        end
+      end
+
+      @total_food_items = 0
+      @total_price = 0
+      @missing_food_items.each do |item|
+        @total_food_items += item.quantity
+        @total_price += Food.find(item.food_id).price
+      end
+
+    else
+      flash[:error] = 'Recipe or inventory not found.'
+      redirect_to root_path
+    end
+  end
+
+  def default
+    @recipes = current_user.recipes
+    @recipes_food = []
+    @recipes.each do |item|
+      @recipes_food += item.recipe_foods
+    end
+
+    @inventories = current_user.inventories
+    @inventories_food = []
+    @inventories.each do |item|
+      @inventories_food += item.inventory_foods
+    end
+
+    @missing_food_items = @recipes_food - @inventories_food
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'shopping_lists/index'
   devise_for :users
   get 'up' => 'rails/health#show', as: :rails_health_check
 
@@ -10,4 +11,8 @@ Rails.application.routes.draw do
     resources :inventory_foods, only: [:index, :destroy]
     resources :foods, only: [:new, :create]
   end
+
+  get '/shopping_lists', to: 'shopping_lists#index', as: 'shopping_lists'
+  get '/shopping_lists/default', to: 'shopping_lists#default', as: :default_shopping_lists
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
 end
