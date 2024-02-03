6b5af875ed1e58ee98d1f71619767f75cf0a64a8
unknown <macherki.nessrine@yahoo.com>
2024-02-03 18:26:30 +0100

    update tests files

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :recipe do
-    
+    name { Faker::Food.dish }
+    preparation_time { Faker::Number.decimal(l_digits: 1, r_digits: 2) }
+    cooking_time { Faker::Number.decimal(l_digits: 1, r_digits: 2) }
+    description { Faker::Lorem.paragraph }
+    public { true }
+    user
   end
 end
