bded7aa083aaab3a17cf495d53db2d16d7bd6c6b
unknown <macherki.nessrine@yahoo.com>
2024-02-03 10:45:55 +0100

    full shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,6 @@ Rails.application.routes.draw do
   end
 
   get '/shopping_lists', to: 'shopping_lists#index', as: 'shopping_lists'
-
+  get '/shopping_lists/default', to: 'shopping_lists#default', as: :default_shopping_lists
  
 end
