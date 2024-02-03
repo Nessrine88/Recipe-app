59fa1782f0213d16bdc23ab78422e1f62143a90e
unknown <macherki.nessrine@yahoo.com>
2024-02-03 09:49:26 +0100

    fix the shopping list logic

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,53 +1,57 @@
-# app/controllers/shopping_lists_controller.rb
 class ShoppingListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    set_recipe_and_inventory
-    generate_shopping_list
-  end
-
-  private
-
-  def set_recipe_and_inventory
     @recipe = Recipe.find(params[:recipe_id])
     @inventory = Inventory.find(params[:inventory_id])
-  end
 
-  def generate_shopping_list
-    return unless @recipe && @inventory
+    if @recipe && @inventory
+      recipe_food_items = @recipe.recipe_foods
+      inventory_food_items = @inventory.inventory_foods
 
-    recipe_food_items = @recipe.recipe_foods
-    inventory_food_items = @inventory.inventory_foods
+      @missing_food_items = []
 
-    @missing_food_items = find_missing_food_items(recipe_food_items, inventory_food_items)
+      recipe_food_items.each do |item|
+        inventory_item = inventory_food_items.find { |inventory_item| inventory_item.food == item.food }
 
-    calculate_totals
-  end
-
-  def find_missing_food_items(recipe_food_items, inventory_food_items)
-    @missing_food_items = []
-
-    recipe_food_items.each do |item|
-      inventory_item = inventory_food_items.find { |inv_item| inv_item.food == item.food }
-
-      if inventory_item
-        if inventory_item.quantity < item.quantity
-          missing_quantity = item.quantity - inventory_item.quantity
-          missing_item = item.dup
-          missing_item.quantity = missing_quantity
-          @missing_food_items << missing_item
+        if inventory_item
+          if inventory_item.quantity < item.quantity
+            missing_quantity = item.quantity - inventory_item.quantity
+            missing_item = item.dup
+            missing_item.quantity = missing_quantity
+            @missing_food_items << missing_item
+          end
+        else
+          @missing_food_items << item.dup
         end
-      else
-        puts "There's no missing food  "
       end
+
+      @total_food_items = 0
+      @total_price = 0
+      @missing_food_items.each do |item|
+        @total_food_items += item.quantity
+        @total_price += Food.find(item.food_id).price
+      end
+
+    else
+      flash[:error] = 'Recipe or inventory not found.'
+      redirect_to root_path
     end
   end
 
-  def calculate_totals
-    @total_food_items = @missing_food_items.sum(&:quantity)
-    @total_price = @missing_food_items.sum { |item| item.quantity * item.food.price }
-  end
+  def default
+    @recipes = current_user.recipes
+    @recipes_food = []
+    @recipes.each do |item|
+      @recipes_food += item.recipe_foods
+    end
 
-  
+    @inventories = current_user.inventories
+    @inventories_food = []
+    @inventories.each do |item|
+      @inventories_food += item.inventory_foods
+    end
+
+    @missing_food_items = @recipes_food - @inventories_food
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,10 @@ class Food < ApplicationRecord
 
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
+  #validations 
+
+  validates :name, presence: true, uniqueness: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :inventory, presence: true
 end
